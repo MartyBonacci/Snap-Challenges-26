8fc839bd7f52cfb86814c30cfc593536de5e6609
martybonacci <martybonacci@gmail.com>
2019-10-09 09:55:40 -0600

    made a couple functions

---
+++ snap-10-9/script.js
@@ -0,0 +1,15 @@
+function calculatePeperoniNeeded(pizzas, peperoniPerPizza) {
+	return pizzas * peperoniPerPizza;
+}
+
+console.log(calculatePeperoniNeeded(3,7))
+
+
+
+
+function concatStrings(firstName, lastName) {
+	return firstName + ' ' + lastName;
+	// return firstName.concat(lastName);
+}
+
+console.log(concatStrings('Mayla', 'Bonacci'))
